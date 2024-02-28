77dbf3919d4d34a7a3b91840d8d0cfa50a8d4391
khusanov-95 <fa.khusano@gmail.com>
2024-02-28 11:19:34 +0500

    inintial

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,6 +1,7 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
 import { graphql } from 'graphql';
+console.log(123123);
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   fastify.route({
